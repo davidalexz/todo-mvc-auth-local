947012bd4b670932f258a6f17e001c58f0c58488
davidalexz <alexz.chimi@gmail.com>
2023-02-26 16:39:19 +0200

    new changes to config

--- controllers/todos.js
+++ controllers/todos.js
@@ -2,7 +2,8 @@ const Todo = require('../models/Todo')
 
 module.exports = {
     getTodos: async (req,res)=>{
-        console.log(req.user)
+        // console.log(req.user)
+		console.log(req)
         try{
             const todoItems = await Todo.find({userId:req.user.id})
             const itemsLeft = await Todo.countDocuments({userId:req.user.id,completed: false})
